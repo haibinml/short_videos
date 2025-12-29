41b3deb69b90f1c2306a480437cfd028504e9946
JH-Ahua <55637210+jiuhunwl@users.noreply.github.com>
2025-12-29 17:17:39 +0800

    使用cloudflare代理视频

--- api/douyin.php
+++ api/douyin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @Author: JH-Ahua
- * @CreateTime: 2025/10/11 上午12:42
+ * @CreateTime: 2025/12/29 上午17:17
  * @email: admin@bugpk.com
  * @blog: www.jiuhunwl.cn
  * @Api: api.bugpk.com
@@ -95,7 +95,7 @@ function douyin($url)
             'title' => $videoDetail['desc'],
             'cover' => $videoDetail['video']['cover'],
             'images' => $images,
-            'url' => $url,
+            'url' => 'https://svproxy.168299.xyz/?proxyurl=' . base64_encode($url),
             'music' => [
                 'title' => $videoDetail['music']['musicName'] ?? null,
                 'author' => $videoDetail['music']['ownerNickname'] ?? null,
